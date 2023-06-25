715085117d1deec37a7431b4dc4ebf503feca901
SeoJiWon <sjiwon4491@gmail.com>
2023-06-25 18:31:21 +0900

    feat: 게임 프로세싱을 위한 GameController 및 Application 동작 처리

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,67 @@
+package baseball.controller;
+
+import baseball.model.*;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+
+import static baseball.model.GameProcessDecider.GAME_RESTART;
+import static baseball.model.GameStatus.GAME_RUNNING;
+import static baseball.model.GameStatus.GAME_TERMINATE;
+
+public class GameController {
+    private static GameStatus gameStatus;
+    private Computer computer;
+    private User user;
+
+    public GameController() {
+        computer = new Computer();
+        gameStatus = GAME_RUNNING;
+    }
+
+    public void run() {
+        // 게임 시작
+        OutputView.printGameStart();
+
+        while (gameStatus.isGameNotTerminated()) {
+            // User - Baseball 입력
+            readUserBaseballInput();
+
+            // 게임 결과 확인
+            Result result = judgeGameByReferee();
+            OutputView.printGameResult(result);
+
+            // 게임 클리어 확인
+            checkGameClear(result);
+        }
+    }
+
+    public void readUserBaseballInput() {
+        List<Integer> userBaseballs = InputView.readUserBaseballInput();
+        user = new User(userBaseballs);
+    }
+
+    private Result judgeGameByReferee() {
+        return Referee.judge(computer.getBaseballs(), user.getBaseballs());
+    }
+
+    private void checkGameClear(final Result result) {
+        if (result.isGameClear()) {
+            OutputView.printGameClear();
+            determineGameRestartOrEnd();
+        }
+    }
+
+    private void determineGameRestartOrEnd() {
+        int userCommand = InputView.readUserRestartCommandInput();
+        GameProcessDecider decider = GameProcessDecider.getDecider(userCommand);
+
+        if (decider == GAME_RESTART) {
+            computer = new Computer();
+            gameStatus = GAME_RUNNING;
+        } else {
+            gameStatus = GAME_TERMINATE;
+        }
+    }
+}
