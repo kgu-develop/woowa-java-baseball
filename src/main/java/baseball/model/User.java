4aa97f212fda38efec6f6e8fa07ba4e668975449
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-03 10:44:03 +0900

    refactor(User) : 하드코딩 했던 것 상수로 리팩토링

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -4,6 +4,10 @@ package baseball.model;
 import java.util.List;
 
 public class User {
+
+    public static final String INPUT_NUMBER_LENGTH_NOT_TRHEE_EXCEPTION = "입력 숫자는 3자리입니다.";
+    public static final String INPUT_DIGIT_LENGTH_NOT_ONE_EXCEPTION = "입력 숫자는 3자리입니다.";
+
     public enum RestartStatus { RESTART, TERMINATE }
 
     private List<Integer> number;
@@ -16,11 +20,11 @@ public class User {
 
     public void validateNumber(final List<Integer> number) {
         if (number.size() > 3 || number.size() < 3) {
-            throw new IllegalArgumentException("입력 숫자는 3자리입니다.");
+            throw new IllegalArgumentException(INPUT_NUMBER_LENGTH_NOT_TRHEE_EXCEPTION);
         }
 
         if (!number.stream().allMatch(digit -> digit >= 1 && digit <= 9)) {
-            throw new IllegalArgumentException("입력 숫자는 3자리입니다.");
+            throw new IllegalArgumentException(INPUT_DIGIT_LENGTH_NOT_ONE_EXCEPTION);
         }
     }
 
