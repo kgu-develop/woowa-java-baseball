4c2425886f554a9f33bcb21bcfc5d8b7bc05662e
SangBeom-Hahn <hsb990917@gmail.com>
2023-07-03 10:37:19 +0900

    refactor(InputView) : 하드코딩 했던 것 view 분리 및 상수로 리팩토링

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,34 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputView {
+
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String STRING_REGEX = "[-+]?\\d*\\.?\\d+";
+    public static final String RESTART_OR_END_NUMBER_NOT_NUMBER_EXCEPTION = "재시작은 1, 완전 종료는 2인 정수입니다.";
+
+    public static List<Integer> inputNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
+        String inputNumber = Console.readLine();
+        List<Integer> number = new ArrayList<>();
+
+        for (int i = 0; i < inputNumber.length(); i++) {
+            char digitNumber = inputNumber.charAt(i);
+            String digitStringNumber = String.valueOf(digitNumber);
+
+            validateNotStringRestartStatus(digitStringNumber);
+            number.add(Integer.valueOf(digitStringNumber));
+        }
+        return number;
+    }
+
+    public static void validateNotStringRestartStatus(final String restartStatus) {
+        if ( !(restartStatus != null && restartStatus.matches(STRING_REGEX)) ) {
+            throw new IllegalArgumentException(RESTART_OR_END_NUMBER_NOT_NUMBER_EXCEPTION);
+        }
+    }
+}
